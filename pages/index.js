fbb268ff7bea18295611a1329adf9985478e1a65
Daniel Schmitz <danielschmitz.arts@gmail.com>
2020-01-18 15:34:06 +0100

    + Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default () => (
+    <div>
+        <Header/>
+        <h1>Test</h1>
+        <Footer/>
+    </div>
+)
